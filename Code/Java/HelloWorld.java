80ab3abd47b00350496fa8bd35c0190491d350d6
coffee2theorems <coffee2theorems314@gmail.com>
2016-11-10 21:08:31 -0600

    Test Commit

--- Code/Java/HelloWorld.java
+++ Code/Java/HelloWorld.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class HelloWorld{
   public static void main(String[] args){
     System.out.println("Hello World!");
+    System.out.print("Enter a word :: ");
+    Scanner kb = new Scanner(System.in);
+    String word = kb.nextLine();
+    System.out.println("You entered :: "+word);
   }
 }
