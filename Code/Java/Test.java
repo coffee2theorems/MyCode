3d7266c19f723a7823374fb8b91730e23f2ec186
Ryan Monaghan <coffee2theorems314@gmail.com>
2017-06-08 17:53:16 -0500

    updating files

---
+++ Code/Java/Test.java
@@ -0,0 +1,17 @@
+public class Test{
+   public static void main(String[] args){
+      System.out.println("Hello World");
+   }
+}
+
+public static int binarySearch(int key, int[] intArray){
+   int low = 0;
+   int high = intArray.length -1;
+   while(low <= high){
+      int mid = low + (high - low)/2;
+      if(key <intArray[mid])
+      {
+      
+      }
+   }
+}
