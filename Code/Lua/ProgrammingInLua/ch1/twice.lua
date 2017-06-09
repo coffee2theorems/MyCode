a74e715ff3506d88075693c6865c21ec0151df65
Ryan Monaghan <coffee2theorems314@gmail.com>
2017-06-09 12:15:59 -0500

    Addes commit to functions

--- Code/Lua/ProgrammingInLua/ch1/twice.lua
+++ Code/Lua/ProgrammingInLua/ch1/twice.lua
@@ -1,7 +1,7 @@
-function norm (x, y)
+function norm (x, y) --Finds the norm of two numbers
     return math.sqrt(x^2 + y^2)
 end
 
-function twice (x)
+function twice (x) --doulbes a number
     return 2.0 * x
 end
